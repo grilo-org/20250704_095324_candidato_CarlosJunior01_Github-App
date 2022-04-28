e78e2e165e887f7af4afe99b19d6fb6eef17c4dd
Carlos Junior <carlos.udi@live.com>
2022-04-28 01:51:21 -0300

    Map response object from github api repositories
    The response mapping of the api repositories was done.

---
+++ core/src/main/java/com/carlos/core/data/network/response/RepositoriesResponse.kt
@@ -0,0 +1,29 @@
+package com.carlos.core.data.network.response
+
+import com.google.gson.annotations.SerializedName
+
+data class RepositoriesResponse(
+    @SerializedName("total_count") val totalCount: Int?,
+    @SerializedName("items") val items: List<Items?>
+)
+
+data class Items(
+    @SerializedName("id") val id: Int?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("full_name") val fullName: String?,
+    @SerializedName("owner") val owner: Owner?,
+    @SerializedName("description") val description: String?,
+    @SerializedName("created_at") val createdAt: String?,
+    @SerializedName("updated_at") val updatedAt: String?,
+    @SerializedName("stargazers_count") val stargazersCount: Int?,
+    @SerializedName("watchers_count") val watchersCount: Int?,
+    @SerializedName("forks_count") val forksCount: String?,
+    @SerializedName("language") val language: String?
+)
+
+data class Owner(
+    @SerializedName("login") val login: String?,
+    @SerializedName("id") val id: String?,
+    @SerializedName("avatar_url") val avatarUrl: String?,
+    @SerializedName("html_url") val htmlUrl: String?
+)
